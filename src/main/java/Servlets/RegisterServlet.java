58f942714d748aeea99aa95f2a42cb1c4760e315
Aliya <a11ya94@mail.ru>
2017-03-15 20:32:55 +0300

    inputig of data in RegisterServlet and creating the new User

--- src/main/java/Servlets/RegisterServlet.java
+++ src/main/java/Servlets/RegisterServlet.java
@@ -3,6 +3,7 @@ package Servlets;
 import dao.H2.H2UserDao;
 import dao.UserDao;
 import model.User;
+import sun.util.calendar.BaseCalendar;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
@@ -12,48 +13,39 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
-import java.util.Optional;
+import java.sql.Date;
+import java.time.LocalDate;
 
 @WebServlet(name = "RegisterServlet")
 public class RegisterServlet extends HttpServlet {
-  /*  public static final String LOGIN = "Welcome";
-    public static final String PASSWORD = "AllGuns";*/
     public static final String USER = "User";
     private UserDao userDao;
     @Override
     public void init(ServletConfig config) throws ServletException {
        userDao=(UserDao)config.getServletContext().getAttribute("UserDao");
-        //gunDao = (GunDao) config.getServletContext().getAttribute("GunDao");
     }
-    /*protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        doPost(request, response);
-    }
-*/
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String firstname=request.getParameter("firstname");
-        String lastname=request.getParameter("lastname");
+        String firstname=request.getParameter("firstName");
+        String lastname=request.getParameter("lastName");
         String patronymic=request.getParameter("patronymic");
+        String gender=request.getParameter("gender");
         String nickname=request.getParameter("nickname");
-        /*String dob=request.getParameter("dob").toLocalDate();
+        Date date=Date.valueOf(request.getParameter("dob"));
+        LocalDate dob=date.toLocalDate();
         String telephone=request.getParameter("telephone");
         String email=request.getParameter("email");
         String password=request.getParameter("password");
+        Double height=Double.valueOf(request.getParameter("height"));
+        Double weight=Double.valueOf(request.getParameter("weight"));
+        String country=request.getParameter("country");
+        String city=request.getParameter("city");
         String status=request.getParameter("status");
-        User user=new User(2,firstname,lastname,patronymic,nickname,telephone,email,password,status);
-        userDao.create(user);*/
-
-
-        request.setAttribute(USER,userDao.getAll());
-        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/registration/login.jsp");
+        int rating=0;
+        User user=new User(1,firstname,lastname,patronymic,gender,nickname,dob,telephone,email,password,height,weight,country,city,status,rating);
+        System.out.print(user.toString());
+        userDao.create(user);
+       // request.setAttribute(USER,userDao.getAll());
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/login.jsp");
         requestDispatcher.forward(request, response);
-        /*String login = request.getParameter("login");
-        String password=request.getParameter("password");
-        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/registration/login.jsp");
-        request.setAttribute(LOGIN, login);
-        request.setAttribute(PASSWORD, password);
-        requestDispatcher.forward(request, response);*/
     }
 }
--- src/main/java/model/Status.java
+++ src/main/java/model/Status.java
@@ -1,5 +1,5 @@
 package model;
 
 public enum Status {
-    TRAINER, SPORTSMAN, UNKNOWN;
+    Sportsman,Trainer,Unknown
 }
